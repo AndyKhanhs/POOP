8b79aac631b108953d394bbf0b493ca570cc3e86
Admin <Admin@KHANHLINH>
2023-11-11 09:40:36 +0700

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using System.Windows;
+
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string question, string defaultAnswer = "")
+        {
+            InitializeComponent();
+            lbQuestion.Content = question;
+            txtAnswer.Text = defaultAnswer;
+        }
+
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            txtAnswer.SelectAll();
+            txtAnswer.Focus();
+        }
+
+        public string Answer
+        {
+            get { return txtAnswer.Text; }
+        }
+    }
+}
---
+++ WpfApp2/Window1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void btnEnterName_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow inputDialog = new MainWindow("Please enter your name:", "John Doe");
+            if (inputDialog.ShowDialog() == true)
+                lblName.Text = inputDialog.Answer;
+        }
+    }
+}
